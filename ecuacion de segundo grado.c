9ff2c7618981a753a373b3adb8f9c45c9b08f2fc
jodegracia26 <jlr2611@outlook.com>
2021-07-26 14:29:40 -0500

    first commit

---
+++ ecuacion de segundo grado.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void ecuaciones (double, double, double, double *, double *);
+
+void main (void){
+    double a, b, c, x1, x2;
+
+    printf("Introduzca a: \n");
+    scanf("%lf", &a);
+    printf("Introduzca b: \n");
+    scanf("%lf", &b);
+    printf("Introduzca c: \n");
+    scanf("%lf", &c);
+    
+}
